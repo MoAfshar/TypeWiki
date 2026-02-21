b26a752fffe68f496ceac9303a7ef54ef94466f4
Mo Afshar <moafshaar@gmail.com>
2026-02-21 17:30:09 +0000

    Add embedding model and pinecone config

--- src/typewiki/config.py
+++ src/typewiki/config.py
@@ -4,4 +4,6 @@ from pydantic_settings import BaseSettings
 
 class TypeWikiConfig(BaseSettings):
     openai_model_name: str = 'gpt-5-mini'
+    openai_embedding_model_name: str = 'text-embedding-3-large'
     openai_api_key: SecretStr
+    pinecone_api_key: SecretStr
